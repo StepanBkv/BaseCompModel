bb835e8feed31f04bb67b17c20a2569156698ff0
Stepan Babakov <stepan.babakov.1999@mail.ru>
2021-05-06 11:59:44 +0300

    Остался интерфейс

--- Lab_2_MonteKarlo.py
+++ Lab_2_MonteKarlo.py
@@ -23,32 +23,35 @@ def opredel_integral(a, b, n):
     return ((b - a) / n) * sum([formula(random.uniform(float(a), float(b))) for i in range(n)])
 
 
-def monte_karlo_1():
-    n = 100000
-    x = [i / 1000000.0 for i in range(0, 20000000)]
-    # список с значениями float из которого будут выбираться случайный значения x
+def monte_karlo_1(n=1000):
+    x = [i / 100.0 for i in range(0, 2000)]
+    # y = [(lambda x: 10 * x / 2 if 0 <= x < 2 else 10 * ((x - 20) / -18) if 2 <= x < 20 else 0)(random.uniform(0, 10))
+    #      for i in range(n)]
     y = [10 * x[i] / 2 if 0 <= x[i] < 2 else 10 * ((x[i] - 20) / -18) if 2 <= x[i] < 20 else 0 for i in
-         range(0, 2000000)]
-    # список с значениями float из которго будут выбираться случайные значения y
-    # print({x[i]: y[i] for i in range(2000000)})
-    random_list_y = [i / 1000000.0 for i in range(0, 10000000)]
-    t = {j: (random.choice(x), random.choice(random_list_y)) for j in range(n)}
-    N = [
-        True if (0 <= t[key][0] < 2 and t[key][1] < (10 * t[key][0]) / 2) or (2 <= t[key][0] < 20 and t[key][1] < 10 * (
-                t[key][0] - 20) / -18) else False for key in t]
+         range(0, 2000)]
+    t = {i: (random.uniform(0.0, 20.0), random.uniform(0.0, 10.0)) for i in range(n)}
+    got = [[t[key][0], t[key][1]] for key in t if
+           ((0 <= t[key][0] < 2) and (t[key][1] < (10 * t[key][0]) / 2)) or (
+                   (2 <= t[key][0] < 20) and (t[key][1] < 10 * (t[key][0] - 20) / -18))]
+    not_got = [[t[key][0], t[key][1]] for key in t if
+               ((0 < t[key][0] <= 2) and (t[key][1] >= (10 * t[key][0]) / 2)) or (
+                       (2 < t[key][0] <= 20) and (t[key][1] >= 10 * (t[key][0] - 20) / -18))]
+    print(not_got)
     plt.figure(figsize=(12, 8))
     plt.title("Monte - Karlo")
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.autoscale(tight=True)
     legend = []
-    plt.plot([t[key][0] for key in t], [t[key][1] for key in t], 'D')  # постановка случайных точкек на плоскость
-    plt.plot(x, y, 'D')  # график пересекающихся функций
+    plt.scatter([key[0] for key in got], [key[1] for key in got], s=5, c="green")
+    # постановка случайных точкек на плоскость
+    plt.scatter([key[0] for key in not_got], [key[1] for key in not_got], s=5, c="red")
+    plt.plot(x, y)  # график пересекающихся функций
     legend.append("Случайные точки")
     # вывод площади треугольника с помощью двух методов Монте - Карло
     legend.append(
-        f"Площадь треугольника S = {(len([i for i in N if i]) / len(N)) * 20 * 10} |"
-        f" {(20 / n) * sum([random.choice(y) for i in range(n)])}")
+        f"Площадь треугольника S = {(len(got) / (len(got) + len(not_got))) * 20 * 10} |"
+        f" {(20 / n) * sum([y[i] for i in range(n)])}")
     # вывод...
     # ограничивающие линии
     plt.plot([0, 20], [10, 10])
@@ -61,22 +64,14 @@ def monte_karlo_1():
     plt.show()
 
 
-def monte_karlo_2():
-    a = 36
-    b = 18
-    n = 10000000
-    random_list = [i / 10000.0 for i in range(1000000)]
-    # x = [i / 1000000.0 for i in range(0, 36000000)]
-    # y = [i / 1000000.0 for i in range(0, 18000000)]
-    # t = {j: (random.choice(x), random.choice(y)) for j in range(n)}
-    # method = "random.choise"
+def monte_karlo_2(n=100, a=36, b=18):
+    random_list = [i / 10.0 for i in range(1000)]
     t = {j: (random.uniform(0.0, 36.0), random.uniform(0.0, 18.0)) for j in range(n)}
-    method = "random.uniform"
     got = [[t[key][0], t[key][1]] for key in t if (10 * ((t[key][0] - 20) / -8) + 20 > t[key][1] < 10 * t[key][0] / 12)]
     dot_got = [[t[key][0], t[key][1]] for key in t if
                (10 * ((t[key][0] - 20) / -8) + 20 <= t[key][1] or t[key][1] >= 10 * t[key][0] / 12)]
-    def_1 = [10 * random_list[i] / 12 for i in range(1000000)]
-    def_2 = [10 * ((random_list[i] - 20) / -8) + 20 for i in range(1000000)]
+    def_1 = [10 * random_list[i] / 12 for i in range(1000)]
+    def_2 = [10 * ((random_list[i] - 20) / -8) + 20 for i in range(1000)]
 
     plt.figure(figsize=(12, 8))
     plt.title("Monte - Karlo")
@@ -88,9 +83,9 @@ def monte_karlo_2():
     legend.append("Случайные точки")
     f = (len(got) / (len(got) + len(dot_got))) * a * b
     print(len(got) / len(got) + len(dot_got))
-    s = (18 * 36) / 2
+    s = (a * b) / 2
 
-    legend.append(f"Площадь треугольника {method} S = {f}, {s}, Погрешность:{abs(s - f) / f}")
+    legend.append(f"Площадь треугольника S = {f}, {s}, Погрешность:{abs(s - f) / f}")
     plt.scatter([key[0] for key in got], [key[1] for key in got], s=5, c="blue")
     plt.scatter([key[0] for key in dot_got], [key[1] for key in dot_got], s=5, c="green")
     plt.plot(random_list, def_1)
@@ -99,20 +94,20 @@ def monte_karlo_2():
           f"Мат.ожидание X: {numpy.nanvar([t[key][0] for key in t])}")
     print(f"Дисперсия Y: {numpy.nanmean([t[key][1] for key in t])}",
           f"Мат.ожидание Y: {numpy.nanvar([t[key][1] for key in t])}")
-    print(f"Вычисление дисперсии по формулам X: {18}",
-          f"Вычисление мат.ожидания X: {36 ** 2 / 12}")
+    print(f"Вычисление дисперсии по формулам X: {b}",
+          f"Вычисление мат.ожидания X: {a ** 2 / 12}")
     print(f"Вычисление дисперсии по формулам Y: {9}",
-          f"Вычисление мат.ожидания Y: {18 ** 2 / 12}")
-    plt.plot([0, 36], [0, 0])
-    plt.plot([0, 36], [18, 18])
-    plt.plot([36, 36], [0, 18])
-    plt.plot([0, 0], [0, 18])
+          f"Вычисление мат.ожидания Y: {b ** 2 / 12}")
+    plt.plot([0, a], [0, 0])
+    plt.plot([0, a], [b, b])
+    plt.plot([a, a], [0, b])
+    plt.plot([0, 0], [0, b])
     plt.legend(legend, loc="upper left")
     plt.grid()
     plt.show()
 
 
-def circle(r, n):
+def circle(n, r):
     plt.figure(figsize=(12, 8))
     plt.title("Monte - Karlo")
     plt.xlabel("X")
@@ -129,7 +124,7 @@ def circle(r, n):
     plt.plot([r, r], [-r, r])
     t = {j: (random.uniform(-2.0, 2.0), random.uniform(-2.0, 2.0)) for j in range(n)}
     got = [[t[key][0], t[key][1]] for key in t if ((t[key][0])) ** 2 + (t[key][1]) ** 2 <= r ** 2]
-    dot_got = [[t[key][0], t[key][1]] for key in t if (abs(t[key][0])) ** 2 + (abs(t[key][1])) ** 2 > r ** 2]
+    dot_got = [[t[key][0], t[key][1]] for key in t if (t[key][0]) ** 2 + (t[key][1]) ** 2 > r ** 2]
 
     plt.scatter([key[0] for key in got], [key[1] for key in got], s=5, c="blue")
     plt.scatter([key[0] for key in dot_got], [key[1] for key in dot_got], s=5, c="brown")
@@ -182,4 +177,4 @@ def figur(n, a, b):
     plt.show()
 
 
-figur(1000000, 13, 9)
+
