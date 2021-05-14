1da3baf36ee6ef9f1548e25c3b2c0e09e76a221d
Stepan <stepan.babakov.1999@mail.ru>
2021-05-14 08:52:53 +0300

    Рандом 1-3

--- Lab_3_Random_chisla.py
+++ Lab_3_Random_chisla.py
@@ -19,31 +19,45 @@ legend = []
 
 def random_def_1(n=10000):
     y = 0
+    y_list = [y]
     for i in range(n):
         y_1 = formula(y) % 1
         y = y_1
-        print(y)
+        y_list.append(y)
+    plt.plot([x for x in range(n+1)], y_list)
+    plt.legend(legend, loc="upper left")
+    plt.grid()
+    plt.show()
 
 
-def random_def_2(n=10000):
+def random_def_2(n=100):
     y = [((2 ** 30) * x / (n * 10)) % 1 for x in range(1, n)]
-    print(y)
+    plt.plot([x for x in range(1, n)], y)
+    plt.legend(legend, loc="upper left")
+    plt.grid()
+    plt.show()
 
 
 def rand_num_Neiman(n=5):
     y = 7153
+    y_list = [y / (10 ** 4)]
     for i in range(n):
         y = ((y ** 2) // 10 ** 2) % 10 ** 4
-        print(y / (10 ** 4))
-
+        y_list.append(y / (10 ** 4))
+    plt.plot([x for x in range(n+1)], y_list)
+    plt.legend(legend, loc="upper left")
+    plt.grid()
+    plt.show()
 
 def random_def_4(n=100):
     m_1 = 1357
-    m_l = []
+    m_l = [m_1/1000]
     for i in range(n):
-        m_1 = ((1357 * m_1) % 5689)
-        m_l.append(m_1 / 10000)
-        print(m_l)
+        m_1 = ((1357 * m_1) / 5689)
+        m_l.append(m_1 / 1000)
+    plt.plot([x for x in range(n + 1)], m_l)
+    plt.legend(legend, loc="upper left")
+    plt.grid()
+    plt.show()
 
-
-rand_num_Neiman(1)
+random_def_4(10)
