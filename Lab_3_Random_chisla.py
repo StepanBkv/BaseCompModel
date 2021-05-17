272d387904912e26c36040082c3a9fd5bc8ac5a6
Stepan Babakov <stepan.babakov.1999@mail.ru>
2021-05-17 22:03:48 +0300

    переделать

--- Lab_3_Random_chisla.py
+++ Lab_3_Random_chisla.py
@@ -1,12 +1,5 @@
 import random
 import matplotlib.pyplot as plt
-import numpy
-import math
-
-
-def formula(x):
-    return math.exp(x ** 2 + x + 5)
-
 
 plt.figure(figsize=(12, 8))
 plt.title("Monte - Karlo")
@@ -14,27 +7,6 @@ plt.xlabel("X")
 plt.ylabel("Y")
 plt.autoscale(tight=False)
 legend = []
-
-
-def random_def_1(n=10000):
-    y = 0
-    y_list = [y]
-    for i in range(n):
-        y_1 = formula(y) % 1
-        y = y_1
-        y_list.append(y)
-    plt.hist(y_list)
-    plt.legend(legend, loc="upper left")
-    plt.grid()
-    plt.show()
-
-
-def random_def_2(n=100):
-    y = [((2 ** 30) * x / (n * 10)) % 1 for x in range(1, n)]
-    plt.hist(y)
-    plt.legend(legend, loc="upper left")
-    plt.grid()
-    plt.show()
 
 
 def rand_num_Neiman(n=5):
@@ -51,22 +23,48 @@ def rand_num_Neiman(n=5):
 
 def multi_kong_met(n=100):
     m_1 = 1357
-    m_l = [m_1 / 10000]
+    data = []
     for i in range(n):
-        m_1 = ((1357 * m_1) % 9999)
-        m_l.append(m_1 / 10000)
-    plt.hist(m_l)
+        m_1 = ((1357 * m_1) % 5689)
+        data.append(m_1 / 10000)
+        print(data)
+    # plt.hist(data)
+    # plt.legend(legend, loc="upper left")
+    # plt.grid()
+    # plt.show()
+
+
+def method_proizveden(n=10000):
+    core = 5167
+    multi_plex = 3729
+    data = []
+    for i in range(n):
+        a = (multi_plex * core) % 10 ** 6
+        multi_plex = a % 10 ** 4
+        data += [(a // 10 ** 2) / 10 ** 4]
+    plt.hist(data, color='blue', edgecolor='black', bins=40)
     plt.legend(legend, loc="upper left")
+    plt.tight_layout()
     plt.grid()
     plt.show()
 
 
-n = input("Введите кол-во случайных чисел для 1 метода: ")
-random_def_1(int(n))
-n = input("Введите кол-во случайных чисел для 2 метода: ")
-random_def_2(int(n))
-n = input("Введите кол-во случайных чисел для метода Неймана: ")
-rand_num_Neiman(int(n))
-n = input("Введите кол-во случайных чисел для мультипликативного конгруэнтного метода: ")
-multi_kong_met(int(n))
+def random_def_2(n=100):
+    y = [((2 ** 30) * x / (n * 10)) % 1 for x in range(1, n)]
+    plt.hist(y)
+    plt.legend(legend, loc="upper left")
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+
+
+# n = input("Введите кол-во случайных чисел для 1 метода: ")
+# random_def_1(int(n))
+# n = input("Введите кол-во случайных чисел для 2 метода: ")
+# random_def_2(int(n))
+# n = input("Введите кол-во случайных чисел для метода Неймана: ")
+# rand_num_Neiman(int(n))
+# n = input("Введите кол-во случайных чисел для мультипликативного конгруэнтного метода: ")
+# multi_kong_met(int(n))
 # реализация первых 4 методов практики.
+multi_kong_met(100)
