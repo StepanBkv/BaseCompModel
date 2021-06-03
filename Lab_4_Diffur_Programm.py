9fba538906d3cac30b3d32e2e6f7423d28ae60d3
Stepan Babakov <48482963+StepanBkv@users.noreply.github.com>
2021-06-03 11:54:15 +0300

    4 задача выполнена

--- Lab_4_Diffur_Programm.py
+++ Lab_4_Diffur_Programm.py
@@ -12,8 +12,9 @@ def g(x, y, z):
 
 y_0 = 3.0
 z_0 = 0.0
-end_int = 1 # Последнее значение для построения графиков
-step = 100 # Количество шагов
+
+end_int = input("Введите последнее значение для построения графиков: ") # Последнее значение для построения графиков
+step = input("Введите кол-во шагов (разбиений): ") # Количество шагов
 
 def realY(t):
     return 4 * math.exp(-t) - math.exp(2*t)
@@ -27,39 +28,28 @@ x, h = np.linspace(0, end_int, step, retstep=True)
 y = [y_0]
 z = [z_0]
 
-for i in range(0, len(x) - 1):
+for i in range(len(x) - 1):
 
     i1 = h * f(x[i], y[i], z[i])
     l1 = h * g(x[i], y[i], z[i])
 
-    i2 = h * f(x[i] + 1.0 / 2.0 * h, y[i] + 1.0 / 2.0 * i1, z[i] + 1.0 / 2.0 * l1)
-    l2 = h * g(x[i] + 1.0 / 2.0 * h, y[i] + 1.0 / 2.0 * i1, z[i] + 1.0 / 2.0 * l1)
+    i2 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * i1, z[i] + 0.5 * l1)
+    l2 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * i1, z[i] + 0.5 * l1)
 
-    i3 = h * f(x[i] + 1.0 / 2.0 * h, y[i] + 1.0 / 2.0 * i2, z[i] + 1.0 / 2.0 * l2)
-    l3 = h * g(x[i] + 1.0 / 2.0 * h, y[i] + 1.0 / 2.0 * i2, z[i] + 1.0 / 2.0 * l2)
+    i3 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * i2, z[i] + 0.5 * l2)
+    l3 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * i2, z[i] + 0.5 * l2)
 
     i4 = h * f(x[i] + h, y[i] + i3, z[i] + l3)
     l4 = h * f(x[i] + h, y[i] + i3, z[i] + l3)
 
-    dy = 1.0 / 6.0 * (i1 + 2*i2 + 2*i3 + i4)
-    dz = 1.0 / 6.0 * (l1 + 2*l2 + 2*l3 + l4)
+    dy = (1.0 / 6.0) * (i1 + 2*i2 + 2*i3 + i4)
+    dz = (1.0 / 6.0) * (l1 + 2*l2 + 2*l3 + l4)
 
     y.append(y[i] + dy)
     z.append(z[i] + dz)
 
-# График функции Y
-#
-# plt.plot(x, [realY(i) for i in x])
-# plt.plot(x, y)
-# plt.grid(True)
-#
-# # График функции Z
-#
-# plt.plot(x, [realZ(i) for i in x])
-# plt.plot(x, z)
-# plt.grid(True)
 
-# Сводный график уравнения
+# График уравнения
 plt.plot(x, [realY(i) for i in x], c="blue")
 plt.plot(x, y, c="blue", ls="--")
 plt.plot(x, [realZ(i) for i in x], c="red")
